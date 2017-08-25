53c9d124e3052468b5f2d8fedf89a8f78c6556ed
Andrew Vargas <avargas011@student.butte.edu>
2017-08-25 02:04:08 +0000

    Lab1.1 submission

---
+++ Lab11/lab11.cpp
@@ -0,0 +1,12 @@
+User starts the number guessing game program
+User will have a total of ten points
+Program chooses one random number between 1-25
+User attempts to guess that number
+Program tells user 'higher' if they guessed a lower number
+Program tells user 'lower' if they guessed a higher number
+Program subracts one out of a total of ten points if they guess incorrectly
+User guesses again
+Program tells user 'higher' or 'lower' depending on user's choice of number
+User will continue to guess
+Program will continue to subtract from the user's ten points if they guess incorrectly
+User guesses correctly
