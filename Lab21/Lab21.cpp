72f0aa8b6d42956b6c6b5b9f210c6a39bbf2675a
Andrew Vargas <avargas011@student.butte.edu>
2017-09-15 03:11:58 +0000

    Lab2.1 Submission

---
+++ Lab21/Lab21.cpp
@@ -0,0 +1,53 @@
+/* This program is to output specific formulas on windchills. 
+
+
+
+*/ 
+
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+   
+  double temp = 0.0; 
+  
+  double oldstyle = 0.0; 
+  double newstyle = 0.0;
+  
+  double velocity = 0.0;
+  double difference = 0.0; 
+  
+  
+  
+  oldstyle = 0.081 * (3.17 * sqrt(velocity)) + (5.81 - 0.25 * velocity) * (temp - 91.4) + 91.4;
+  
+  
+  newstyle = (35.74 + 0.6125) * (temp - 35.75 * pow (velocity, 0.16 )) + (0.4275 * temp * pow (velocity, 0.16));
+  
+  
+  
+  difference = oldstyle / newstyle;
+  
+  
+  
+  cout << velocity << oldstyle << newstyle << difference << endl; 
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
