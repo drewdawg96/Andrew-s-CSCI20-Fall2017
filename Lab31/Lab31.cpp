87a6c17fd08c5233cf3153b0f7ee35d9eb51f8e8
Andrew Vargas <avargas011@student.butte.edu>
2017-10-17 22:27:34 +0000

    Lab3.1 Submission

--- Lab31/Lab31.cpp
+++ Lab31/Lab31.cpp
@@ -5,26 +5,51 @@ Date: 9/26/2017
 */
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+
+int main() 
+{
+    int attPlus; // So you say I need these, but why exactly? 
+    int attChoice;
+    int attFam;
+    int verizonU;
+    int SprintU;
+    int SprintGb;
     
-    int user_Phones = 0;
-    int user_lines = 0; 
-    int user_Data = 0;
-    int userPlan = 0;
+int user_lines; 
+int user_tablet; 
+int user_data; 
+int user_price; 
     
-    // How many people? phones? data? 
+    cout << "How many GB of data will you require? If you recommend unlimited data, type in 0"; // This is where I want the user to input their information?
+    cin >> user_data;
     
+    cout << "How many phone lines will you require? "; // Also am I missing any important data they need to be asked? like minimum or maximum price? 
+    cin >> user_lines;
     
-    if (user_Phones < 10) {
-        user_lines = 
+    cout << "How many tablets do you require? ";
+    cin >> user_tablet;
     
+    if (user_data > 0) 
+    {
+        cout << "Exactly how much data will you require: "; // I know this is probably wrong, but this is where I am really confused??
+        cin >> user_data;  
         
+    if (user_data >= 1) && (user_data <= 10) 
+        cout << "We recommend... "
     }
     
+    if (user_data == 0)
+        cout << "How much are you willing to spend? "; 
+        cin >> user_price;
     
-    // absoulutely lost and stuck on my equations... trying to fix ASAP
+    
+    
+    
+    cout << "How many lines will you require? Max is 10";  
+    
     
     
     
@@ -37,8 +62,7 @@ int main() {
     
     
     return 0;
-    
-    }   
+}   
         
     
     
